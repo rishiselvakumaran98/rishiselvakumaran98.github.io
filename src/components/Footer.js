1505d4ade9c8a3aaa5c061f65767263d1cb1b31f
Rishi Selvakumaran <selva053@umn.edu>
2021-09-02 17:06:19 -0500

    Modified Footer for app

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -14,7 +14,7 @@ const Footer = () => (
 			<h2>Contact info</h2>
 			<dl class="alt">
 				<dt>Address</dt>
-				<dd>930 University Ave SE &bull; Minneapolis, MN 55414 &bull; USA</dd>
+				<dd>2601 University Ave SE &bull; Minneapolis, MN 55414 &bull; USA</dd>
 				<dt>Email</dt>
 				<dd><a href="#">rishi.selvakumaran@gmail.com</a></dd>
 			</dl>
@@ -22,7 +22,7 @@ const Footer = () => (
 				<li><a href="https://www.facebook.com/rishiselvakumaran/" class="icon brands fa-facebook-f alt"><span class="label">Facebook</span></a></li>
 				<li><a href="https://www.instagram.com/rishiselvakumaran/" class="icon brands fa-instagram alt"><span class="label">Instagram</span></a></li>
 				<li><a href="https://github.com/rishiselvakumaran98" class="icon brands fa-github alt"><span class="label">GitHub</span></a></li>
-				<li><a href="www.linkedin.com/in/rishi-selvakumaran-a35892130" class="icon brands fa-linkedin alt"><span class="label">LinkedIn</span></a></li>
+				<li><a href="https://www.linkedin.com/in/rishi-selvakumaran-a35892130" class="icon brands fa-linkedin alt"><span class="label">LinkedIn</span></a></li>
 			</ul>
 		</section>
 		<p class="copyright">&copy; Rishi's Portfolio Site. Design: <a href="https://html5up.net">HTML5 UP</a>.</p>
