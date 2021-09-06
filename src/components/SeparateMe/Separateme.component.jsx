636e899646c6de606f594a2b430586055ee2b3d3
Rishi Selvakumaran <selva053@umn.edu>
2021-09-05 23:17:18 -0500

    Modified: SeparateMe Component

--- src/components/SeparateMe/Separateme.component.jsx
+++ src/components/SeparateMe/Separateme.component.jsx
@@ -7,18 +7,21 @@ const SeparateMe = () => {
               <h2>What separates me from the rest?</h2>
             </header>
             <p>
-              When developing software applications, I always drive the extra mile to carefully research and look closely for use cases 
-              that needs to satisfied for the users of my applications and this drives me to make compelling software applications
-              that encompasses clients's needs carefully while delivering reusable components for future development needs. 
-              Every application has its own unique set of use cases, and it is the duty of a good developer to identify them,
-              and fix them ASAP. As a software developer thats where I shine in fulfilling them. As philosophers say, travel alone to reach fast, 
-              travel together to reach further, I aspire to reach further in my career by working in a team setting 
-              and I always look for opportunities to learn from my teammates. I also spearhead projects and make sure that 
-              my team moves in the pace that is best suited for everyone and also achieve goals that help our project and team standout from the rest. 
-              As a developer, who highly values ethics in Technology, I also pay careful attention to the right features and flexibility for clients/users 
-              to modify the state of the application such as being able to delete or update their posts in a blogging website (Check my CS-Insiders). 
-              Hence these characteristics give me an edge in practicing the best software techniques while also constantly updating myself
-              to develop the best applications.
+            When developing software applications, I always drive the extra mile to carefully research 
+            and examine use cases that need to satisfied for users of my applications. This persistence 
+            drives me to make compelling software applications that encompass clients's needs carefully 
+            while delivering reusable components for future development needs. Every application has its own 
+            unique set of use cases, and it is the duty of a good developer to identify them, and fix them 
+            ASAP. As a software developer, that is where I shine in fulfilling them. As philosophers say: 
+            travel alone to go fast, travel together to go further. I aspire to reach further in my career 
+            by working in a team setting and I always look for opportunities to learn from my teammates. 
+            I also spearhead projects and make sure that my team moves in the pace that is best suited for 
+            everyone. With this approach, my team and I have achieved goals that have helped our project 
+            and team stand out from the rest. As a developer who highly values ethics in technology, I also 
+            pay careful attention when adding features so that I can provide flexibility for clients/users to 
+            modify the state of the application such as being able to delete or update their posts in a blogging 
+            website (Check my CS-Insiders). Hence, these characteristics give me an edge in practicing the best software 
+            techniques while also constantly updating myself to develop the best applications.
             </p>
           </div>
     )
